8bd9ee95acfe521e7f4632265368fd813ef340fc
Alina Mozol <alina@astrabit.io>
2025-02-15 16:36:31 +0200

    Get prices

---
+++ index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+(()=> {
+	console.log(123);
+
+	function getPricesHistory () {
+		// Promise<[number, number][]> => {
+	  // let res: [number, number][] = [];
+
+	  try {
+	    const data =
+	      (
+	        await axios.get(
+				'https://api.coingecko.com/api/v3/coins/usd-coin/market_chart?vs_currency=usd&days=1CG-piYAkfuLU4agtL2vEyUqRiP5'
+	        ).data || {};
+
+	    if (data && data.prices) {
+	      res = data.prices;
+	    }
+	  } catch (err) {
+	    logger.error("getPricesHistory error", { err });
+	  }
+
+	  // return res;
+	};
+	} 
+})()
